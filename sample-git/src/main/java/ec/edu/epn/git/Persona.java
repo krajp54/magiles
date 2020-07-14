345f7d103f6555399e15ac56f9d72cae713fe5c9
Juan Pablo Rodriguez <rodriguezjp4@gmail.com>
2020-07-14 15:15:04 -0500

    Primer Commit

---
+++ sample-git/src/main/java/ec/edu/epn/git/Persona.java
@@ -0,0 +1,30 @@
+package ec.edu.epn.git;
+
+public class Persona {
+
+	private int age;
+	private String name;
+
+	public Persona(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ sample-git/src/test/java/ec/edu/epn/git/PersonaTest.java
@@ -0,0 +1,14 @@
+package ec.edu.epn.git;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonaTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
